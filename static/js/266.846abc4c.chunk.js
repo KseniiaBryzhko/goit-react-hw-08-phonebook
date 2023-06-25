1d27fa9abd0a51848c363ca69d866cd8bf52be42
KseniiaBryzhko <114442785+KseniiaBryzhko@users.noreply.github.com>
2023-06-25 12:01:24 +0000

    Deploying to gh-pages from @ KseniiaBryzhko/goit-react-hw-08-phonebook@3afdf773bc4efbaf26db6ccbd9322149b76a6d49 🚀

---
+++ static/js/266.846abc4c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,r,n){n.r(r),n.d(r,{default:function(){return t}});var a=n(184);function t(){return(0,a.jsx)("div",{children:(0,a.jsxs)("h1",{children:["Task manager welcome page"," ",(0,a.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=266.846abc4c.chunk.js.map
